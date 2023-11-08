62cea6fc19dd2ab842ab1c6a601fae9e7c100a99
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-08 16:53:10 +0100

    Scaffolding

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      title: "La mia prima app con Vite"
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="title">
+      <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<style lang="">
+</style>
